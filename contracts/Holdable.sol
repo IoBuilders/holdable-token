2c5a24fc5ecb15616be75b2dae76b0993da3b4d2
Daniel Lehrner <daniel@io.builders>
2019-11-22 12:35:57 +0100

    npm package updates, uses string utils library instead of own contract

--- contracts/Holdable.sol
+++ contracts/Holdable.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IHoldable.sol";
-import "./libraries/StringUtil.sol";
+import 'solidity-string-util/contracts/StringUtil.sol';
 
 
 contract Holdable is IHoldable, ERC20 {
