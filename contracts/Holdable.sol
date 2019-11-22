f19d9a7cdada9c4b640b8c930e2a2aab0a52087b
Daniel Lehrner <daniel@io.builders>
2019-11-22 12:43:39 +0100

    fix linter issues

--- contracts/Holdable.sol
+++ contracts/Holdable.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IHoldable.sol";
-import 'solidity-string-util/contracts/StringUtil.sol';
+import "solidity-string-util/contracts/StringUtil.sol";
 
 
 contract Holdable is IHoldable, ERC20 {
